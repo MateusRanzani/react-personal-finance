0caa894df05f84f251f5e0f53b5c64592696f3d0
Mateus Ranzani <mateusranzani@hotmail.com>
2022-06-11 14:29:48 -0300

    first commit

---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.main `
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 2.4rem 1rem;
+
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle `
+    :root {
+        --background: #f0f2f5;
+        --red: #E52E4D;
+        --blue: #5429CC;
+        --green: #33CC95;
+
+        --blue-light: #6933FF;
+
+        --text-tittle: #363F5F;
+        --text-body: #969C83;
+
+        --background: #f8f2f5;
+        --shape: #FFFFFF;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%
+        }
+
+        @media (max-width: 720px) {
+            font-size: 83.5%
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-aliased;
+    }
+
+
+`
