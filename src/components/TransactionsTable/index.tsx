8f8f81fdb17422b66a33919611cd7e5b18a32a36
MateusRanzani <mateusranzani@hotmail.com>
2022-06-16 10:55:59 -0300

    creating the route and adding the transactions to the transaction table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,58 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface TransactionsInterface {
+  id: number;
+  title: string;
+  type: string;
+  category: string;
+  amount: number;
+  createdAt: string;
+}
 
 export function TransactionsTable() {
-    
-useEffect(() => {
-    api.get("/transactions")
-    .then(response => console.log(response.data))
-}, [])
+  const [transactions, setTransactions] = useState<TransactionsInterface[]>([]);
 
+  useEffect(() => {
+    api
+      .get("/transactions")
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
 
   return (
     <Container>
-        <table>
-            <thead>
-                <tr>
-                    <th>Título</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Datas</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        Desenvolvimento de website
-                    </td>
-                    <td  className="deposit">
-                        R$12.000
-                    </td>
-                    <td>
-                        Desenvolvimento
-                    </td>
-                    <td>
-                        20/02/2021
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Aluguel 
-                    </td>
-                    <td className="withdraw">
-                        - R$1.100
-                    </td>
-                    <td>
-                        Casa
-                    </td>
-                    <td>
-                        17/02/2021
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Datas</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Container>
   );
 }
